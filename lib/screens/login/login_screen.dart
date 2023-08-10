2525db382bb3514d4a1a3a9c230b13ab7e668f2b
Erhauyi-ovie <ovieeugene53@gmail.com>
2023-08-10 14:00:12 +0100

    updated login screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,185 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:solar_project/screens/home/home.dart';
-
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 300, // Adjust the height as needed
-              child: FittedBox(
-                fit: BoxFit.fitHeight,
-                child: Image.asset('assets/images/solars.jpg'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 250, left: 170),
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 23,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 330, left: 55),
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Text(
-                  'Email',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 360, left: 55),
-              child: SizedBox(
-                height: 60,
-                width: 300,
-                child: Container(
-                  height: 60,
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Enter password',
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue, width: 1.0),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 450, left: 55),
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Text(
-                  'Password',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 490, left: 55),
-              child: SizedBox(
-                height: 60,
-                width: 300,
-                child: Container(
-                  height: 60,
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Enter password',
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue, width: 1.0),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 550, left: 60),
-              child: SizedBox(
-                height: 60,
-                width: 300,
-                child: Container(
-                  height: 60,
-                  width: 300,
-                  child: Row(
-                    children: [
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          'Contact Admin',
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 630, left: 45),
-              child: SizedBox(
-                height: 60,
-                width: 300,
-                child: Container(
-                  height: 60,
-                  width: 300,
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 35),
-                        child: Container(
-                          height: 50,
-                          width: 250,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Home()),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.green,
-                            ),
-                            child: Text(
-                              'Login',
-                              style: TextStyle(color: Colors.white, fontSize: 25),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Stack(
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
               children: [
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  child: SizedBox(
-                    width: 415,
-                    height: 345,
-                    // child: Image.asset('assets/images/solars.jpg'),
+                Container(
+                  width: size.width*1,
+                  height: 280,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.asset("assets/images/solar.jpg"),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 40, top: 40),
+                  child: Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            child: Text("Email", style: TextStyle(color: Colors.blue, fontSize:20),),
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                hintText: "Enter Email",
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blue)
+                              )
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const SizedBox(
+                            child: Text("Password", style: TextStyle(color: Colors.blue, fontSize:20),),
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                hintText: "Enter Password",
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue)
+                                ),
+                                suffixIcon: IconButton(
+                                  onPressed: null,
+                                  icon: Icon(Icons.remove_red_eye_sharp),
+                                )
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 10, bottom: 10),
+                                child: Text('Forgot Password?', style: TextStyle(color: Colors.blue, fontSize:20),),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: Text('Contact Admin',style: TextStyle(color: Colors.blue, fontSize:20)),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 70,
+                            width: 330,
+                            child: ElevatedButton(
+                              onPressed: () {
+                              },
+                              child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 20),),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
